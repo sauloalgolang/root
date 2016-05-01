e645ba401d627894cd711517d73707826846d937
Saulo Alves <sauloal@gmail.com>
2016-05-02 00:26:57 +0200

    added test

---
+++ S_lycopersicum_chromosomes.2.50.fa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+FA=S_lycopersicum_chromosomes.2.50.fa
+URL=ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/assembly/build_2.50/
+
+if [[ ! -f "$FA" ]]; then
+	wget ${URL}/${FA}
+fi
+
+#./run.sh github.com/sauloalgolang/fastareader/fastareader/fastareader.go ${FA}
+
+./install.sh github.com/sauloalgolang/fastareader/fastareader
+
+./install.sh github.com/sauloalgolang/fastareader/fastaindexer
+
+bin/fastareader ${FA}
